0f218e01769f26f51e38fc6e04d112406050530a
8qiqi <3510363360@qq.com>
2019-04-17 12:13:28 +0800

    jsnotepad转到对话框

--- dlg-goto/dlg-goto.js
+++ dlg-goto/dlg-goto.js
@@ -1,79 +1,62 @@
-/* exported $dlgGoto */
-var $dlgGoto = (function() {
-  var $dlg = $(''
-        + '<div class="notepad-dlg-mask notepad-dlg-goto">'
-          + '<div class="dialogbox notepad-dlgbox">'
-            + '<div class="notepad-dlg-titlebar">'
-              + '<p class="title">转到指定行</p>'
-              + '<span class="close-btn">✖</span>'
-            + '</div>'
-            + '<div class="main notepad-dlg-main">'
-              + '<label for="">行号(L):</label><br>'
-              + '<input class="txt-line-num" type="text" autofocus><br>'
-              + '<input class="btn-goto btn" type="button" value="转到">'
-              + '<input class="btn-cancel btn" type="button" value="取消">'
-            + '</div>'
-          + '</div>'
-        + '</div>');
-
-  var $btnClose = $dlg.find('.close-btn'),
-      $btnCancel = $dlg.find('.btn-cancel'),
+/*exported $dlgGoto*/
+var $dlgGoto = (function(){
+  var DOM=''
+    +'<div class="notepad-dlg notepad-goto-dlg">'
+    +'  <div class="dialogbox">'
+    +'    <div class="titlebar">'
+    +'      <p class="title">转到指定行</p>'
+    +'      <span class="btn-close">×</span>'
+    +'    </div>'
+    +'    <div class="content">'
+    +'      <label for="">行号(L):</label>'
+    +'      <input type="text" class="line-num" autofocus/>'
+    +'      <input type="button" value="转到" class="btn-goto"/>'
+    +'      <input type="button" value="取消" class="btn-cancel"/>'
+    +'    </div>'
+    +'  </div>'
+    +'</div>';
+  
+  var $dlg = $(DOM),
       $btnGoto = $dlg.find('.btn-goto'),
-      $txtLineNum = $dlg.find('.txt-line-num'),
-      $titleBar = $dlg.find('.notepad-dlg-titlebar');
-
-  var cfg = {
-    lineNum: 1,
-    totalLine: 1,
-    gotoHandler: null
-  };
-
-  function destoryDlg() { $dlg.remove(); }
-
-  function gotoHandler() {
-    if(!validateLineNum()) return;
-
-    cfg.gotoHandler($txtLineNum.val()); 
-    destoryDlg();
-  }
-
-  function filterKey(e) {
-    if(!/[0-9]/.test(e.key)) {
-      e.preventDefault();
-      showErrMsg();
-    }
-  }
-
-  function showErrMsg() {
-    alert('show Error Message!');   // TODO: 需要改成气泡框提示错误信息
-  }
-
-  function validateLineNum() {
-    var n = Number($txtLineNum.val());
-
-    if(n === 0 || n > cfg.totalLine) {
-      alert('行数超过了总行数');  // TODO：需要改成自己的对话框
-      $txtLineNum.select();
+      $lineNum = $dlg.find('.line-num'),
+      $btnCancel = $dlg.find('.btn-cancel'),
+      $btnClose = $dlg.find('.btn-close');
+  
+  function validation(){
+    if(!/[0-9]/.test($lineNum.val())){
+      alert('请输入数字');
       return false;
-    } else {
-      return true;
     }
+    if(cfg.curLine>cfg.totalLine){
+      alert('超出最大行数');
+      return false;
+    }
+    return true;
   }
-
-  function show(conf) {
-    $.extend(cfg, conf);
-
+  function gotoHandler(){
+    if(!validation()) return;
+    cfg.gotoHandler(cfg.curLine);
+    $dlg.remove();
+  }
+  var cfg={
+    curLine:1,
+    totalLine:10,
+    gotoHandler:gotoHandler
+  };
+  function destory(){
+    $dlg.remove();
+  }
+  function show(conf){
+    cfg = conf;
     $('body').append($dlg);
-    $dlg.find('.dialogbox').draggable({handle: $titleBar});
-
-    $btnClose.click(destoryDlg);
-    $btnCancel.click(destoryDlg);
+    $lineNum.val(cfg.curLine);
+    $lineNum.select();
+   
+    $dlg.find('.dialogbox').draggable({handle:$dlg.find('.titlebar')});
     $btnGoto.click(gotoHandler);
-    $txtLineNum.keypress(filterKey);
-
-    $txtLineNum.val(cfg.lineNum);
-    $txtLineNum.select();
+    $btnClose.click(destory);
+    $btnCancel.click(destory);
   }
+  return {show:show};
 
-  return {show: show};
-})();
+}());
